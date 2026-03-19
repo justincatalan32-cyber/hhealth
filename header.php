1f8b952a0005aaff38cbde78410e4b3308d3c900
justincatalan32-cyber <justincatalan32@gmail.com>
2026-03-19 13:16:32 +0800

    Update header.php

--- header.php
+++ header.php
@@ -2,44 +2,51 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gapan City Health Center</title>
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
 
-<div class="container mt-4">
-  <div class="card">
+<nav class="navbar navbar-expand-lg navbar-custom shadow-sm">
+  <div class="container">
 
-    <div class="card-header d-flex align-items-center justify-content-between">
+    <a class="navbar-brand d-flex align-items-center" href="index.php">
+      <img src="logo.jpg" width="60" height="60" class="me-2">
+      Gapan City Health Center
+    </a>
 
-      <div>
-        <img src="logo.jpg" width="200" alt="Logo">
-      </div>
-
-      <ul class="nav nav-pills nav-fill">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+                        
+    <div class="collapse navbar-collapse justify-content-center" id="navbarContent">
+      <ul class="navbar-nav gap-4">
         <li class="nav-item">
-          <a class="nav-link active" href="#">Home</a>
+          <a class="nav-link active" href="index.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Doctor Schedule</a>
+          <a class="nav-link" href="doctor_schedule.php">Doctor Schedule</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Services</a>
+          <a class="nav-link" href="services.php">Services</a>
         </li>
-         <li class="nav-item">
-            <a class="nav-link" href="login.php">Log In</a>
-         </li>
-         <li class="nav-item">
-            <a class="nav-link" href="register.php">Register/Sign Up</a>
-         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="news.php">News & Announcements</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="login.php">Log In</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="register.php">Register/Sign Up</a>
+        </li>
       </ul>
-
     </div>
 
   </div>
-</div>
+</nav>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
